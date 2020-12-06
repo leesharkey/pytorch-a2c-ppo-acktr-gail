f3973e7d8e1a93b047e95c7786710811f1653b3b
leedsharkey@gmail.com <leedsharkey@gmail.com>
2020-12-06 12:18:00 +0100

    Removed gym from setup because we're using a local version instead

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ setup(
     name='a2c-ppo-acktr',
     packages=find_packages(),
     version='0.0.1',
-    install_requires=['gym', 'matplotlib', 'pybullet'])
+    install_requires=['matplotlib', 'pybullet'])
+    # Removed gym as a dependency since we're using a local version here
