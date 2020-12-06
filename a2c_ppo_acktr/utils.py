f62e6213dcff1619592242ecd8d82ad0e09a4d61
leedsharkey@gmail.com <leedsharkey@gmail.com>
2020-12-06 12:17:21 +0100

    Added functions for saving configs to csv for record keeping

--- a2c_ppo_acktr/utils.py
+++ a2c_ppo_acktr/utils.py
@@ -1,11 +1,11 @@
 import glob
 import os
-
 import torch
 import torch.nn as nn
-
 from a2c_ppo_acktr.envs import VecNormalize
-
+import pandas as pd
+import os
+from datetime import datetime
 
 # Get a render function
 def get_render_func(venv):
@@ -56,10 +56,78 @@ def init(module, weight_init, bias_init, gain=1):
     return module
 
 
-def cleanup_log_dir(log_dir):
-    try:
-        os.makedirs(log_dir)
-    except OSError:
-        files = glob.glob(os.path.join(log_dir, '*.monitor.csv'))
-        for f in files:
-            os.remove(f)
+def datetimenow(subseconds=False):
+    if not subseconds:
+        now = datetime.now()
+        return now.strftime("%Y%m%d-%H%M%S")
+    else:
+        return datetime.utcnow().strftime('%Y%m%d-%H%M%S-%f')
+
+def save_configs_to_csv(args, session_name=None, model_name=None,
+                        unique_id=None, results_dict=None):
+    """"""
+    arg_dict = vars(args).copy()
+    arg_dict = {**arg_dict, **{'unique_id': unique_id,
+                               'model_name': model_name,
+                               'session_name': session_name}}
+
+    # Combine dicts
+    if results_dict is not None:
+        arg_dict = {**arg_dict, **results_dict}
+
+    # Convert any lists into string so csvs can take them
+    for k, v in arg_dict.items():
+        arg_dict[k] = str(v) if type(v) ==list or type(v) ==dict else v
+
+    # Check there isn't already a df for this unique_id; adjust model name if so
+    # if os.path.isfile("exps/params_and_results_%s.csv" % unique_id) and loading:
+    #     model_name = model_name + 'loaded_' + session_name
+
+    # Create a df with a single row with new info
+    full_df = pd.DataFrame(arg_dict, index=[unique_id])
+
+    # Create a new csv if one doesn't already exist and save the new data
+    params_dir = "../exps/params_and_results"
+    if not os.path.isdir(params_dir):
+        os.mkdir(params_dir)
+    save_filename = "%s.csv" % unique_id
+    full_df.to_csv(os.path.join(params_dir, save_filename))
+    print("Saved params and results csv for %s." % unique_id)
+
+def combine_all_csvs(directory_str, base_csv_name='params_and_results.csv',
+                     remove_old_csvs=False):
+    """Run this using python"""
+    # Define some fixed variables
+    archive_dir = 'archive_csvs'
+    archive_path = os.path.join(directory_str, archive_dir)
+    dfs = []
+    files = [f for f in os.listdir(directory_str) if
+             os.path.isfile(os.path.join(directory_str, f))]
+
+    # Make archive file if keeping old csvs and it doesn't already exist
+    if remove_old_csvs and not os.path.isdir(archive_path):
+        os.mkdir(archive_path)
+
+    # Loop through files, checking if they're a csv, and adding their pandas
+    # dataframe to the list of dfs
+    full_df = None
+    for f in files:
+        ext_filename = os.path.join(directory_str, f) #extended_filename
+        print(ext_filename)
+        if f.endswith('.csv'):
+            new_df = pd.read_csv(ext_filename, header=0, index_col=0)
+            dfs += [new_df]
+            if full_df is not None:
+                full_df = pd.concat([full_df, new_df], axis=0, sort=True)
+            else:
+                full_df = new_df
+            if remove_old_csvs:
+                os.rename(ext_filename, os.path.join(archive_path, f))
+
+    # Concatenate all the dfs together, remove any duplicate rows, and save
+    if dfs==[]:
+        raise FileNotFoundError("No CSVs in the list to be merged. Check" +
+                                " that your path names are correct and that" +
+                                " the folder contains CSVs to merge.")
+    full_df = full_df.drop_duplicates()
+    full_df.to_csv(os.path.join(directory_str, base_csv_name))
